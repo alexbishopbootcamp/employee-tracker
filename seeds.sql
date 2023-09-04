62744021dd6b59b1d281f0eb29b45c690b8c2425
Alex <github@alexbi.shop>
2023-09-04 14:54:35 +0900

    Added database constructor

---
+++ seeds.sql
@@ -0,0 +1,50 @@
+CREATE DATABASE employee_db;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10,2) NOT NULL,
+  department_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
+
+INSERT INTO department (name)
+VALUES ('Sales'), ('Engineering'), ('Finance'), ('Legal');
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('Sales Lead', 100000.00, 1),
+       ('Salesperson', 80000.00, 1),
+       ('Lead Engineer', 150000.00, 2),
+       ('Software Engineer', 120000.00, 2),
+       ('Account Manager', 160000.00, 3),
+       ('Accountant', 125000.00, 3),
+       ('Legal Team Lead', 250000.00, 4),
+       ('Lawyer', 190000.00, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('John', 'Doe', 1, NULL),
+       ('Mike', 'Chan', 2, 1),
+       ('Ashley', 'Rodriguez', 3, NULL),
+       ('Kevin', 'Tupik', 4, 3),
+       ('Kunal', 'Singh', 5, NULL),
+       ('Malia', 'Brown', 6, 5),
+       ('Sarah', 'Lourd', 7, NULL),
+       ('Tom', 'Allen', 8, 7);
