f97c0989ae78746aee9204dc2805d5d1b92f1a94
Alex <github@alexbi.shop>
2023-09-04 16:23:43 +0900

    Moved SQL related code into sql.js. Removed direct access to dangerous rebuildDatabase() func from index.js

--- index.js
+++ index.js
@@ -1,59 +1,9 @@
 const inquirer = require('inquirer');
-const sql = require('mysql2');
-const fs = require('fs');
+const sql = require('./sql/sql.js');
 
-const db = sql.createConnection(
-  {
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'toor',
-    multipleStatements: true, // For parsing the seeds.sql file
-  },
-  console.log('Connected to database.')
-);
-
-// Build template database from seeds.sql
-function rebuildDatabase(){
-    db.query('DROP DATABASE IF EXISTS employee_db', (err, res) => {
-        if (err) throw err;
-        console.log('Dropped database employee_db');
-    });
-    db.query(fs.readFileSync('./seeds.sql').toString(), (err, res) => {
-        if (err) throw err;
-        console.log('Created tables');
-    });
-    db.end();
-}
-
-function startup(){
-
-  // Check if employee_db exists, rebuild if necessary
-  db.query('SHOW DATABASES', (err, res) => {
-    if (err) throw err;
-    if (res.some(entry => entry.Database === 'employee_db')) {
-      console.log('Database employee_db exists');
-    } else {
-      console.log('Database employee_db does not exist');
-      rebuildDatabase();
-    }
-  });
-
-  db.query('USE employee_db', (err, res) => {
-    if (err) throw err;
-    console.log('Using employee_db');
-  });
-}
-
-function shutdown(){
-  db.end();
-  console.log('Disconnected from database.');
-}
 
 function app(){
-  startup();
-
+  sql.startup();
 }
-
 
 app();
